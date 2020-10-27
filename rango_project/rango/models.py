d7c82bc5e97ad437bc2c2497a8a64637917e944b
Vagif <z.b.2021@list.ru>
2020-10-27 10:45:46 +0400

    Modified templates to display posts from database

--- rango_project/rango/models.py
+++ rango_project/rango/models.py
@@ -5,16 +5,16 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(null=True, blank= False)
-def __str__(self):
-   return self.question_text
+    def __str__(self):
+       return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-def __str__(self):
-   return self.question
+    def __str__(self):
+       return self.question
 
-def publish(self):
-    self.pub_date=timezone.now()
-    self.save()
+    def publish(self):
+       self.pub_date=timezone.now()
+       self.save()
--- rango_project/rango/views.py
+++ rango_project/rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from.models import Question
+from django.utils import timezone
 
 def home(request):
-    return render(request, "rango/home.html",{})
+    posts = Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request, "rango/home.html",{'posts':posts})
 
